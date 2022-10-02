2a415ff19ffd57f913a9d8c3078455ec2cdf1f0d
michinoins <>
2022-10-02 12:43:30 -0700

    fix: trust proxy

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.enable('trust proxy');
+app.set('trust proxy', 1);
 app.use(session(ses_opt));
 
 // common processing
